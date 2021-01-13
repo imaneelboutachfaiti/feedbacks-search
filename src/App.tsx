895442d1e7e2133ddbd7c35489c7c7f97b436d91
Imane El Boutachfaiti <imane.elboutachfaiti@gmail.com>
2021-01-13 11:17:13 +0100

    Fetch data from usabilla Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Input from "./Components/Input/Input";
+import Header from "./Components/Header/Header";
+
+interface feedbacks {
+  rating: number;
+  comment: string;
+  device: string;
+  platform: string;
+  browser: browser;
+}
+interface browser {
+  appCodeName: string;
+}
 
 function App() {
+  const [feedbackData, setFeedbackData] = useState<feedbacks[]>([]);
+  useEffect(() => {
+    FetchFeedbacks();
+  }, []);
+  const FetchFeedbacks = async () => {
+    const response = await fetch(
+      "https://static.usabilla.com/recruitment/apidemo.json"
+    );
+    const jsonData = await response.json();
+    setFeedbackData(jsonData.items);
+  };
+  const listItems = feedbackData.map((feedback) => (
+    <div className="feedback-element">
+      <span>{feedback.rating}</span>
+      <span>{feedback.comment}</span>
+      <span>{feedback.browser.appCodeName}</span>
+    </div>
+  ));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <div>
+        <Input placeholder="Search here!"></Input>
+      </div>
+      <div className="feedback-element table-space">
+        <span >Rating</span>
+        <span>Comment</span>
+        <span>browser</span>
+      </div>
+      <div className="advanced-table">{listItems}</div>
     </div>
   );
 }
