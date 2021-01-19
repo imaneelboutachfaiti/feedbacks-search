880300ca2e2d5e06a9d8caffaef6e70fab2ecee8
Imane El Boutachfaiti <imane.elboutachfaiti@gmail.com>
2021-01-19 11:25:05 +0100

    UI Improvements

--- src/App.tsx
+++ src/App.tsx
@@ -22,50 +22,48 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div className ="main-content">
-        <div className="filter-section">
-          <Input onChange={setCriteria} placeholder="Search here!" />
-          <div className="ratings">
-            <RatingButton
-              ratingValue={1}
-              onClick={onRatingClick}
-              selected={ratings.includes(1)}
-            />
-            <RatingButton
-              ratingValue={2}
-              onClick={onRatingClick}
-              selected={ratings.includes(2)}
-            />
-            <RatingButton
-              ratingValue={3}
-              onClick={onRatingClick}
-              selected={ratings.includes(3)}
-            />
-            <RatingButton
-              ratingValue={4}
-              onClick={onRatingClick}
-              selected={ratings.includes(4)}
-            />
-            <RatingButton
-              ratingValue={5}
-              onClick={onRatingClick}
-              selected={ratings.includes(5)}
-            />
-          </div>
-          <div className="devices">
-            <Device imgUrl={smartPhone} type="MOBILE" onClick={setDevice} />
-            <Device imgUrl={monitor} type="DESKTOP" onClick={setDevice} />
-          </div>
+      <div className="filter-section">
+        <Input onChange={setCriteria} placeholder="Search here!" />
+        <div className="ratings">
+          <RatingButton
+            ratingValue={1}
+            onClick={onRatingClick}
+            selected={ratings.includes(1)}
+          />
+          <RatingButton
+            ratingValue={2}
+            onClick={onRatingClick}
+            selected={ratings.includes(2)}
+          />
+          <RatingButton
+            ratingValue={3}
+            onClick={onRatingClick}
+            selected={ratings.includes(3)}
+          />
+          <RatingButton
+            ratingValue={4}
+            onClick={onRatingClick}
+            selected={ratings.includes(4)}
+          />
+          <RatingButton
+            ratingValue={5}
+            onClick={onRatingClick}
+            selected={ratings.includes(5)}
+          />
+        </div>
+        <div className="devices">
+          <Device imgUrl={smartPhone} type="MOBILE" onClick={setDevice} />
+          <Device imgUrl={monitor} type="DESKTOP" onClick={setDevice} />
         </div>
       </div>
       <table>
         <thead className="table-header">
           <tr>
-          <th>Rating</th>
-          <th>Comment</th>
-          <th>platform</th>
-          <th>Browser</th>
-          <th>Device</th>
+            <th>Rating</th>
+            <th>Comment</th>
+            <th>platform</th>
+            <th>Browser</th>
+            <th>Device</th>
           </tr>
         </thead>
         <tbody className="table-content">
